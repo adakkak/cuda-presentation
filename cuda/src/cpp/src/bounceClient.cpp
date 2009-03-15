ec56d31e30035c9a93d483d5fd3983613cb4ff3e
Abdulmajed Dakkak [@Toxic] <adakkak@gmail.com>
2009-03-15 14:31:47 -0500

    works, but the bouncing does not. will probably not fix at this point

--- cuda/src/cpp/src/bounceClient.cpp
+++ cuda/src/cpp/src/bounceClient.cpp
@@ -9,126 +9,158 @@
 #include <transport/TSocket.h>
 #include <transport/TTransportUtils.h>
 
+
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 #include "../../gen-cpp/Split.h"
 
 using namespace std;
-using namespace
-  apache::thrift;
-using namespace
-  apache::thrift::protocol;
-using namespace
-  apache::thrift::transport;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
 
-using namespace
-  split;
+using namespace split;
 
-using namespace
-  boost;
+using namespace boost;
 //
-SplitClient *
-  client;
+SplitClient *client;
 
 // Initial square position and size
-GLfloat
-  x = 0.0f;
-GLfloat
-  y = 0.0f;
-GLfloat
-  rsize = 25;
+GLfloat x = 0.0f;
+GLfloat y = 0.0f;
+GLfloat rsize = 25;
 
 // Step size in x and y directions
 // (number of pixels to move each time)
-GLfloat
-  xstep = 1.0f;
-GLfloat
-  ystep = 1.0f;
+GLfloat xstep = 1.0f;
+GLfloat ystep = 1.0f;
 
 // Keep track of windows changing width and height
-GLfloat
-  windowWidth;
-GLfloat
-  windowHeight;
+GLfloat windowWidth;
+GLfloat windowHeight;
+
 
 ///////////////////////////////////////////////////////////
 // Called to draw scene
-void
-RenderScene (void)
+void RenderScene(void)
 {
-  string
-    s = "";
-  client->display (s);
-  cout << s << endl;
-  // Clear the window with current clearing color
-  glClear (GL_COLOR_BUFFER_BIT);
+    string s;
+    client->display(s);
+    vector < string > splitVec;
+    boost::split(splitVec, s, boost::is_any_of("\n"));
 
-  // Set current drawing color to red
-  //                 R     G         B
-  glColor3f (1.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  // Draw a filled rectangle with current color
-  glRectf (x, y, x + rsize, y - rsize);
+    vector < string > commandVec;
+    for (vector < string >::iterator line = splitVec.begin();
+	 line != splitVec.end(); ++line) {
+	boost::split(commandVec, *line, boost::is_any_of(","));
+	if (commandVec.empty()) {
+	    continue;
+	}
+	if (commandVec[0].compare("color") == 0) {
 
-  // Flush drawing commands and swap
-  glutSwapBuffers ();
+	    float r = boost::lexical_cast < float >(commandVec[1]);
+	    float g = boost::lexical_cast < float >(commandVec[2]);
+	    float b = boost::lexical_cast < float >(commandVec[3]);
+
+	    glColor3f(r, g, b);
+	} else if (commandVec[0].compare("rect") == 0) {
+
+	    float x = boost::lexical_cast < float >(commandVec[1]);
+	    float y = boost::lexical_cast < float >(commandVec[2]);
+	    float w = boost::lexical_cast < float >(commandVec[3]);
+	    float h = boost::lexical_cast < float >(commandVec[4]);
+
+	    glRectf(x, y, w, h);
+	} else {
+	    cout << "Unknown command " << commandVec[0] << endl;
+	}
+    }
+    glutSwapBuffers();
 }
 
 ///////////////////////////////////////////////////////////
 // Called by GLUT library when idle (window not being
 // resized or moved)
-void
-TimerFunction (int value)
+void TimerFunction(int value)
 {
-  client->idle ();
+    client->idle();
+    glutPostRedisplay();
+    glutTimerFunc(33, TimerFunction, 1);
 }
 
 
 ///////////////////////////////////////////////////////////
 // Setup the rendering state
-void
-SetupRC (void)
+void SetupRC(void)
 {
-  // Set clear color to blue
-  glClearColor (0.0f, 0.0f, 1.0f, 1.0f);
+    // Set clear color to blue
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 }
 
 
 ///////////////////////////////////////////////////////////
 // Called by GLUT library when the window has chanaged size
-void
-ChangeSize (int w, int h)
+void ChangeSize(int w, int h)
 {
-  client->reshapeFunc (w, h);
+    cout << "in reshape " << w << ", " << h << endl;
+    client->reshapeFunc(w, h);
+    GLfloat aspectRatio;
+
+    // Prevent a divide by zero
+    if (h == 0)
+	h = 1;
+
+    // Set Viewport to window dimensions
+    glViewport(0, 0, w, h);
+
+    // Reset coordinate system
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // Establish clipping volume (left, right, bottom, top, near, far)
+    aspectRatio = (GLfloat) w / (GLfloat) h;
+    if (w <= h) {
+	windowWidth = 100;
+	windowHeight = 100 / aspectRatio;
+	glOrtho(-100.0, 100.0, -windowHeight, windowHeight, 1.0, -1.0);
+    } else {
+	windowWidth = 100 * aspectRatio;
+	windowHeight = 100;
+	glOrtho(-windowWidth, windowWidth, -100.0, 100.0, 1.0, -1.0);
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 ///////////////////////////////////////////////////////////
 // Main program entry point
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  glutInit (&argc, argv);
-  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowSize (800, 600);
-  glutCreateWindow ("Bounce");
-  shared_ptr < TTransport > socket (new TSocket ("localhost", 9090));
-  shared_ptr < TTransport > transport (new TBufferedTransport (socket));
-  shared_ptr < TProtocol > protocol (new TBinaryProtocol (transport));
-  client = new SplitClient (protocol);
-  try
-  {
-    transport->open ();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("Bounce");
+    shared_ptr < TTransport > socket(new TSocket("localhost", 9090));
+    shared_ptr < TTransport > transport(new TBufferedTransport(socket));
+    shared_ptr < TProtocol > protocol(new TBinaryProtocol(transport));
+    client = new SplitClient(protocol);
+    try {
+	transport->open();
 
-  } catch (TException & tx)
-  {
-    printf ("ERROR: %s\n", tx.what ());
-  }
-  glutDisplayFunc (RenderScene);
-  glutReshapeFunc (ChangeSize);
-  glutTimerFunc (33, TimerFunction, 1);
+    } catch(TException & tx) {
+	printf("ERROR: %s\n", tx.what());
+    }
+    glutDisplayFunc(RenderScene);
+    glutReshapeFunc(ChangeSize);
+    glutTimerFunc(33, TimerFunction, 1);
 
-  SetupRC ();
+    SetupRC();
 
-  glutMainLoop ();
-  transport->close ();
+    glutMainLoop();
+    transport->close();
 
-  return 0;
+    return 0;
 }
--- cuda/src/cpp/src/bounceServer.cpp
+++ cuda/src/cpp/src/bounceServer.cpp
@@ -1,12 +1,15 @@
 // Bounce.cpp
-// Demonstrates a simple animated rectangle program with GLUT
+// Demonstrates a simple animated rectangle 
+// program with GLUT
 // OpenGL SuperBible, 3rd Edition
 // Richard S. Wright Jr.
 // rwright@starstonesoftware.com
 
-#include "shared/gltools.h"	// OpenGL toolkit
+#include <iostream>
+#include <sstream>
+#include <string>
 
-#include "Split.h"
+#include "../../gen-cpp/Split.h"
 #include <protocol/TBinaryProtocol.h>
 #include <server/TSimpleServer.h>
 #include <transport/TServerSocket.h>
@@ -20,144 +23,130 @@ using namespace apache::thrift::server;
 using boost::shared_ptr;
 
 using namespace split;
+using namespace std;
 
 
+
+class SplitHandler:virtual public SplitIf {
+  public:
 // Initial square position and size
-GLfloat x = 0.0f;
-GLfloat y = 0.0f;
-GLfloat rsize = 25;
+    float x;
+    float y;
+    float rsize;
 
 // Step size in x and y directions
 // (number of pixels to move each time)
-GLfloat xstep = 1.0f;
-GLfloat ystep = 1.0f;
+    float xstep;
+    float ystep;
 
 // Keep track of windows changing width and height
-GLfloat windowWidth;
-GLfloat windowHeight;
+    float windowWidth;
+    float windowHeight;
+    SplitHandler() {
+	x = 0.0f;
+	y = 0.0f;
+	rsize = 25;
 
-///////////////////////////////////////////////////////////
-// Called to draw scene
-void
-RenderScene (void)
-{
-  // Clear the window with current clearing color
-  glClear (GL_COLOR_BUFFER_BIT);
-
-  // Set current drawing color to red
-  //                 R     G         B
-  glColor3f (1.0f, 0.0f, 0.0f);
-
-  // Draw a filled rectangle with current color
-  glRectf (x, y, x + rsize, y - rsize);
-
-  // Flush drawing commands and swap
-  glutSwapBuffers ();
-}
-
-///////////////////////////////////////////////////////////
-// Called by GLUT library when idle (window not being
-// resized or moved)
-void
-TimerFunction (int value)
-{
-  // Reverse direction when you reach left or right edge
-  if (x > windowWidth - rsize || x < -windowWidth)
-    xstep = -xstep;
-
-  // Reverse direction when you reach top or bottom edge
-  if (y > windowHeight || y < -windowHeight + rsize)
-    ystep = -ystep;
-
-  // Actually move the square
-  x += xstep;
-  y += ystep;
-
-  // Check bounds. This is in case the window is made
-  // smaller while the rectangle is bouncing and the 
-  // rectangle suddenly finds itself outside the new
-  // clipping volume
-  if (x > (windowWidth - rsize + xstep))
-    x = windowWidth - rsize - 1;
-  else if (x < -(windowWidth + xstep))
-    x = -windowWidth - 1;
-
-  if (y > (windowHeight + ystep))
-    y = windowHeight - 1;
-  else if (y < -(windowHeight - rsize + ystep))
-    y = -windowHeight + rsize - 1;
-
-
-
-  // Redraw the scene with new coordinates
-  glutPostRedisplay ();
-  glutTimerFunc (33, TimerFunction, 1);
-}
-
-
-///////////////////////////////////////////////////////////
-// Setup the rendering state
-void
-SetupRC (void)
-{
-  // Set clear color to blue
-  glClearColor (0.0f, 0.0f, 1.0f, 1.0f);
-}
-
-
-///////////////////////////////////////////////////////////
-// Called by GLUT library when the window has chanaged size
-void
-ChangeSize (int w, int h)
-{
-  GLfloat aspectRatio;
-
-  // Prevent a divide by zero
-  if (h == 0)
-    h = 1;
-
-  // Set Viewport to window dimensions
-  glViewport (0, 0, w, h);
-
-  // Reset coordinate system
-  glMatrixMode (GL_PROJECTION);
-  glLoadIdentity ();
-
-  // Establish clipping volume (left, right, bottom, top, near, far)
-  aspectRatio = (GLfloat) w / (GLfloat) h;
-  if (w <= h)
-    {
-      windowWidth = 100;
-      windowHeight = 100 / aspectRatio;
-      glOrtho (-100.0, 100.0, -windowHeight, windowHeight, 1.0, -1.0);
+// Step size in x and y directions
+// (number of pixels to move each time)
+	xstep = 1.0f;
+	ystep = 1.0f;
     }
-  else
-    {
-      windowWidth = 100 * aspectRatio;
-      windowHeight = 100;
-      glOrtho (-windowWidth, windowWidth, -100.0, 100.0, 1.0, -1.0);
+    
+    float rand_float() {
+	return (float) rand() / RAND_MAX;
+    }
+    ////////////////////////////////////////// Called to draw scene void
+    void display(string & _return) {
+	ostringstream result;
+	result << "color,";
+	result << rand_float() << "," << rand_float() << "," <<
+	    rand_float() << endl;
+
+	result << "rect,";
+	result << x << "," << y << "," << x + rsize << "," << y - rsize;
+
+	string s = result.str();
+	_return = &s[0];
+//      printf("in display\n");
     }
 
-  glMatrixMode (GL_MODELVIEW);
-  glLoadIdentity ();
-}
+    ///////////////////////////////////////////////////////////
+    // Called by GLUT library when idle (window not being
+    // resized or moved)
+    void idle() {
+	// Reverse direction when you reach left or right edge
+	if (x > windowWidth - rsize || x < -windowWidth)
+	    xstep = -xstep;
+
+	// Reverse direction when you reach top or bottom edge
+	if (y > windowHeight || y < -windowHeight + rsize)
+	    ystep = -ystep;
+
+	// Actually move the square
+	x += xstep;
+	y += ystep;
+
+	// Check bounds. This is in case the window is made
+	// smaller while the rectangle is bouncing and the 
+	// rectangle suddenly finds itself outside the new
+	// clipping volume
+	if (x > (windowWidth - rsize + xstep))
+	    x = windowWidth - rsize - 1;
+	else if (x < -(windowWidth + xstep))
+	    x = -windowWidth - 1;
+
+	if (y > (windowHeight + ystep))
+	    y = windowHeight - 1;
+	else if (y < -(windowHeight - rsize + ystep))
+	    y = -windowHeight + rsize - 1;
+//      printf("in idle\n");
+    }
+
+
+    ///////////////////////////////////////////////////////////
+    // Called by GLUT library when the window has chanaged size
+    void reshapeFunc(int16_t w, int16_t h) {
+	windowWidth = w;
+	windowHeight = h;
+	printf("in reshape %d %d\n", w, h);
+    }
+
+    void keyboardEventFunc(const int8_t key, const int16_t x,
+			   const int16_t y) {
+	// Your implementation goes here
+	printf("keyboardEventFunc\n");
+    }
+
+    void mouseEventFunc(const int16_t button, const int16_t x,
+			const int16_t y) {
+	// Your implementation goes here
+	printf("mouseEventFunc\n");
+    }
+
+    void mouseMotionFunc(const int16_t x, const int16_t y) {
+	// Your implementation goes here
+	printf("mouseMotionFunc\n");
+    }
+
+};
 
 ///////////////////////////////////////////////////////////
 // Main program entry point
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  glutInit (&argc, argv);
-  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowSize (800, 600);
-  glutCreateWindow ("Bounce");
-  glutDisplayFunc (RenderScene);
-  glutReshapeFunc (ChangeSize);
-  glutTimerFunc (33, TimerFunction, 1);
+    int port = 9090;
+    shared_ptr < SplitHandler > handler(new SplitHandler());
+    shared_ptr < TProcessor > processor(new SplitProcessor(handler));
+    shared_ptr < TServerTransport >
+	serverTransport(new TServerSocket(port));
+    shared_ptr < TTransportFactory >
+	transportFactory(new TBufferedTransportFactory());
+    shared_ptr < TProtocolFactory >
+	protocolFactory(new TBinaryProtocolFactory());
 
-  SetupRC ();
-
-  glutMainLoop ();
-
-  return 0;
+    TSimpleServer server(processor, serverTransport, transportFactory,
+			 protocolFactory);
+    server.serve();
+    return 0;
 }
